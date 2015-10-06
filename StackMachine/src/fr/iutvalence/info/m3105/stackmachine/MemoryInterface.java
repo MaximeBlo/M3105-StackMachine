ff9d7840ec64927911671d123d5bb2492e85e72b
blouin maxime <maxime.blouin@iut-valence.fr>
2015-10-06 13:02:32 +0200

    edit class with using interface

--- StackMachine/src/fr/iutvalence/info/m3105/stackmachine/MemoryInterface.java
+++ StackMachine/src/fr/iutvalence/info/m3105/stackmachine/MemoryInterface.java
@@ -1,5 +1,11 @@
 package fr.iutvalence.info.m3105.stackmachine;
 
 public interface MemoryInterface {
+	
+	public int getStartAddress();
+	
+	public int getEnAddress();
+
+	public void write(int currentAddress, Object opCode);
 
 }
